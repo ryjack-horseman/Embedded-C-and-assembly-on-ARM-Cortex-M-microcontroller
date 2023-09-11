ffe04124da0e35d67d9e1deb36b5fd20331352af
ryjack-horseman <44657668+ryjack-horseman@users.noreply.github.com>
2023-09-11 17:39:00 -0400

    Refactor and bug fix

--- task_scheduler/Inc/main.h
+++ task_scheduler/Inc/main.h
@@ -1,33 +1,38 @@
-/*
- * main.h
- *
- *  Created on: Sep 11, 2023
- *      Author: ryanjackson
- */
 
 #ifndef MAIN_H_
 #define MAIN_H_
 
-#define MAX_TASKS			4
 
-#define SIZE_TASK_STACK		1024U
-#define SIZE_SCHD_STACK		1024U
+#define MAX_TASKS   4
 
-#define SRAM_START			0x20000000U
-#define SIZE_RAM			((128) * (1024))
-#define SRAM_END			((SRAM_START) + (SIZE_RAM))
+/* some stack memory calculations */
+#define SIZE_TASK_STACK          1024U
+#define SIZE_SCHED_STACK         1024U
 
-#define T1_STACK_START		SRAM_END
-#define T2_STACK_START		(SRAM_END - SIZE_TASK_STACK)
-#define T3_STACK_START		(SRAM_END - (2 * SIZE_TASK_STACK))
-#define T4_STACK_START		(SRAM_END - (3 * SIZE_TASK_STACK))
-#define SCHD_STACK_START	(SRAM_END - (4 * SIZE_TASK_STACK))
+#define SRAM_START               0x20000000U
+#define SIZE_SRAM                ( (128) * (1024))
+#define SRAM_END                 ((SRAM_START) + (SIZE_SRAM) )
 
-#define TICK_HZ				1000U
+#define T1_STACK_START           SRAM_END
+#define T2_STACK_START           ( (SRAM_END) - (1 * SIZE_TASK_STACK) )
+#define T3_STACK_START           ( (SRAM_END) - (2 * SIZE_TASK_STACK) )
+#define T4_STACK_START           ( (SRAM_END) - (3 * SIZE_TASK_STACK) )
+#define IDLE_STACK_START         ( (SRAM_END) - (4 * SIZE_TASK_STACK) )
+#define SCHED_STACK_START        ( (SRAM_END) - (5 * SIZE_TASK_STACK) )
 
-#define HSI_CLOCK			16000000U
-#define SYSTICK_TIM_CLK		HSI_CLOCK
+#define TICK_HZ 1000U
 
-#define DUMMY_XPSR	0x01000000U;
+#define HSI_CLOCK         		16000000U
+#define SYSTICK_TIM_CLK   		HSI_CLOCK
+
+
+#define DUMMY_XPSR  0x01000000U
+
+#define TASK_READY_STATE  0x00
+#define TASK_BLOCKED_STATE  0XFF
+
+#define INTERRUPT_DISABLE()  do{__asm volatile ("MOV R0,#0x1"); asm volatile("MSR PRIMASK,R0"); } while(0)
+
+#define INTERRUPT_ENABLE()  do{__asm volatile ("MOV R0,#0x0"); asm volatile("MSR PRIMASK,R0"); } while(0)
 
 #endif /* MAIN_H_ */
